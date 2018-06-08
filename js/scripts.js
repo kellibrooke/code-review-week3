1afc7826f99dca700b6f1bc8457efb89a1573f2c
Kelli <kellibrookelife@gmail.com>
2018-06-08 12:28:44 -0700

    add master function to link all functions together in order

--- js/scripts.js
+++ js/scripts.js
@@ -25,24 +25,66 @@ function findZeros(number){
   }
 };
 
-function replaceOnes(number){
-  var splitNumber = number.split("");
-  for(x=0; x<splitNumber.length; x++){
-    if(splitNumber[x] === "1"){
-      splitNumber.splice((x+1), 1, "boop");
+function replaceOnes(numberSplit){
+  for(x=0; x<numberSplit.length; x++){
+    if(numberSplit[x] === "1"){
+      numberSplit.splice(x, 1, "boop");
     } else{
-      splitNumber = splitNumber;
+      numberSplit = numberSplit;
     }
   }
-  return splitNumber;
+  return numberSplit;
 }
+
+function replaceZeros(numberSplit){
+  for(x=0; x<numberSplit.length; x++){
+    if(numberSplit[x] === "0"){
+      numberSplit.splice(x, 1, "beep");
+    } else{
+      numberSplit = numberSplit;
+    }
+  }
+  return numberSplit;
+}
+
+function masterFunction(userInput){
+  var splitNumber = userInput.split("");
+  var divisibleTest = divisibleBy3(userInput);
+  var onesTest = findOnes(splitNumber);
+  var zerosTest = findZeros(userInput);
+
+  if(divisibleTest === true){
+    $("#result-box").text("I'm sorry, Dave. I'm afraid I can't do that.");
+  } else if(onesTest === true){
+    var onesDone = replaceOnes(splitNumber);
+    var onesDoneJoined = onesDone.join(" ");
+    if(zerosTest === true){
+      var onesZerosDone = replaceZeros(onesDone);
+      var onesZerosDoneJoined = onesZerosDone.join(" ");
+      $("#result-box").text(onesZerosDoneJoined);
+    } else{
+      $("#result-box").text(onesDoneJoined);
+    }
+  } else if(zerosTest === true){
+    var zerosDone = replaceZeros(splitNumber);
+    var zerosDoneJoined = zerosDone.join(" ");
+    $("#result-box").text(zerosDoneJoined);
+  } else{
+    $("#result-box").text(userInput);
+  }
+
+
+};
 
 
 
 // USER INPUT LOGIC
 $(function(){
   $("#submit-button").click(function(){
-    var uiInput = $("#ui-input");
+    event.preventDefault();
+    var uiInput = $("#ui-input").val();
+    masterFunction(uiInput);
+    document.getElementById("the-form").reset();
   });
 
 
